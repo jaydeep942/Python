052e3207edd18fa7bb38fafdc127119552479e51
Shashwat Chovatiya <shashwatchovatiya316@gmail.com>
2025-08-12 19:03:53 +0530

    All Unit 1 practical done

---
+++ Unit-1/Practical9.py
@@ -0,0 +1,11 @@
+# Write a program to assert the user enters a number greater than zero
+
+
+# Take input from user
+num = float(input("Enter a number greater than zero: "))
+
+# Assert condition
+assert num > 0, "Number must be greater than zero!"
+
+print("You entered:", num)
+
